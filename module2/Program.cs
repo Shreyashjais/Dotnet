8d3bb794a422fe885b29b3e3031845ba4397c917
Shreyash Jaiswal <shreyash.j@kgs.live>
2025-07-30 16:17:55 +0530

    methods and if-else done

---
+++ module2/Program.cs
@@ -0,0 +1,44 @@
+﻿using  System;
+class Program{
+    static void Main(string[] args){
+      //Type Conversion
+      int num= Convert.ToInt32("56");
+      Console.WriteLine(num);
+
+      //Sum of two numbers
+      Console.WriteLine("Enter first number");
+      int num1= Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Enter second number");
+      int num2= Convert.ToInt32(Console.ReadLine());
+      int sum= num1+num2;
+      Console.WriteLine("Sum is "+sum);
+
+      //For Handling decimal numbers
+      //Use Convert.ToDouble 
+
+      //Mad Lib 
+      Console.WriteLine("Enter a color");
+      string color= Console.ReadLine();
+      Console.WriteLine("Enter a plural noun");
+      string pluralNoun= Console.ReadLine();
+      Console.WriteLine("Enter a celebrity name");
+      string celebrity= Console.ReadLine();
+
+
+      Console.WriteLine("Roses are "+ color);
+      Console.WriteLine(pluralNoun + " are blue");
+      Console.WriteLine("I love "+ celebrity);
+
+
+      //Arrays
+      int[] nums= {1, 5, 3, 55, 34, 65, 54};
+      nums[0]=77;
+      Console.WriteLine(nums[0]);
+
+      string[] names= new string[5]; //Array of strings with size 5
+      names[0]= "Jimmy";
+      names[1]= "Robin";
+
+      Console.WriteLine(names[1]);
+    }
+}
---
+++ module3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class Program{
+    static void Main(string[] args){
+      Greet("Shrey", 12);
+      int ans= Cube(3);
+      Console.WriteLine(ans);
+
+      //If Statement
+      bool isMale= false;
+      bool isTall= true;
+      if(isMale && isTall){
+        Console.WriteLine("You are male and tall.");
+      }
+      else if(!isMale && isTall){
+        Console.WriteLine("You are not male but you're tall.");
+      }
+      else{
+        Console.WriteLine("You are female.");
+      }
+
+
+      Console.WriteLine(GreatestNum(5,6));
+
+
+    }
+
+    //Methods
+    static void Greet(string name, int age){
+        Console.WriteLine("Hello "+ name+", how are you?" + " You are " + age);
+    }
+
+    //Method with return value
+    static int Cube(int num){
+        int result= num*num*num;
+        return result;
+    }
+
+    //Greatest of two numbers
+    static int GreatestNum(int a, int b){
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+}
+
