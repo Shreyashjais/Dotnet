9c516b7b7ee971f769c491ac0b099949c9b02a67
Shreyash Jaiswal <shreyash.j@kgs.live>
2025-07-31 18:54:48 +0530

    loops done

--- module3/Program.cs
+++ module3/Program.cs
@@ -21,6 +21,10 @@ class Program{
 
       Console.WriteLine(GreatestNum(5,6));
 
+      Console.WriteLine(Calculator("%", 6,4));
+
+      Console.WriteLine(GetDay(4));
+
 
     }
 
@@ -43,6 +47,69 @@ class Program{
         else{
             return b;
         }
+    }
+
+    //Calculator
+    static int Calculator(string op, int num1, int num2){
+      if(op == "+"){
+        return num1+num2;
+      }
+      else if(op == "-"){
+        return num1-num2;
+      }
+      else if(op == "*"){
+        return num1*num2;
+      }
+      else if(op== "/"){
+        return num1/num2;
+      }
+      else {
+        return num1%num2;
+      }
+     
+    }
+
+
+    //Switch Case
+
+    static string GetDay(int dayNum){
+      string dayName;
+      switch(dayNum){
+        case 0 :
+        dayName="Sunday";
+        break;
+
+        case 1:
+        dayName="Monday";
+        break;
+
+        case 2:
+        dayName="Tuesday";
+        break;
+
+        case 3:
+        dayName="Wednesday";
+        break;
+
+        case 4:
+        dayName="Thursday";
+        break;
+
+        case 5:
+        dayName="Friday";
+        break;
+
+        case 6:
+        dayName="Saturday";
+        break;
+        
+        default:
+        dayName:"Invalid number";
+        break;
+
+
+      }
+      return dayName;
     }
 }
 
---
+++ module4/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+    class Program{
+        static void Main(string[] args){
+          PrintNumbers(8);
+          PrintNums(6);
+          PrintingNums(9);
+
+          Console.WriteLine("Please enter a word");
+          string answer= Console.ReadLine();
+          GuessWord(answer);
+        
+        }
+
+        //While Loop
+        static void PrintNumbers(int index){
+            int i=1;
+            while(i<=index){
+                Console.WriteLine(i);
+                i++;
+            }
+            
+        }
+
+        //Do while loop
+        static void PrintNums(int index){
+            int i=1;
+            do{
+             Console.WriteLine(i);
+            }
+            while(index<=5);
+        }
+
+        //For loop
+        static void PrintingNums(int limit){
+            for(int i=1; i<=limit; i++){
+                Console.WriteLine(i);
+            }
+        }
+
+        //guessing game
+        static void GuessWord(string word){
+            string ans= "Giraffe";
+            while(ans != word){
+                Console.WriteLine("Please guess the word again.");
+                word=Console.ReadLine();
+            }
+            Console.WriteLine("You won!");
+        }
+
+
+        static int GetPow(int)
+    }
